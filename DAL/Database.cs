78a870c474f55b1c15c54f3f88b57c85fd3f590c
Alumnos <Alumnos@LABC2PC9>
2022-09-30 12:07:57 -0600

    Agregar archivos de proyecto.

---
+++ DAL/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practica_guia7.Properties;
+using System.Configuration;
+
+namespace Practica_guia7.DAL
+{
+    public class Database
+    {
+        public static string getStrConnectin()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnectin());
+            return Con;
+        }
+
+    }
+}
